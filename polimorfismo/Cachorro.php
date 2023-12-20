efa93dd19ae2f5e2f3f57045fa1e1d31bc69e5eb
Max Leite <maxlfleite@gmail.com>
2023-12-20 17:44:59 -0300

    Aula Polimorfismo (2)

--- polimorfismo/Cachorro.php
+++ polimorfismo/Cachorro.php
@@ -4,6 +4,46 @@ class Cachorro extends Mamifero {
     public function emitirSom() {
         echo "<p>Au Au Au</p>";
     }
+    // Não existe sobrecarga em PHP
+    // Usar metodos com nomes diferentes resolve esse "problema".
+    public function reacaoFrase($frase){
+        if ($frase == "Comer" || $frase == "Olá") {
+            echo "<p>Inclina a cabeça e abana o rabo</p>";
+        } else {
+            echo "<p>Rosnar</p>";
+        }
+    }
+    public function reacaoHora($hora, $min){
+        if ($hora < 12) {
+            echo "<p>Abanar o rabo</p>";
+        } else if ($hora>= 18){
+            echo "<p>Ignorar</p>";
+        }else {
+            echo "<p>Latir igual a  Tadeu</p>";
+        }
+    }
+    public function reagirDono($dono){
+        if ($dono ) {
+            echo "<p>Abanar o rabo</p>";
+        } else {
+            echo "<p>Rosnar e latir</p>";
+        }
+    }
+    public function reagirIdadePeso(){
+        if ($this->getIdade() <= 5){
+            if ($this->getPeso() <= 10){
+                echo "<p>Abanar a rabeta</p>";
+            } else {
+                echo "<p>Rosnar e latir</p>";
+            }
+        } else {
+            if ($this->getPeso() <= 10){
+                echo "<p>Rosnar e latir</p>";
+            } else {
+                echo "<p>Ignorar</p>";
+            }
+        }
+    }
 
 }
 ?>
--- polimorfismo/index.php
+++ polimorfismo/index.php
@@ -18,6 +18,7 @@
     require_once "Cachorro.php";
     require_once "Tartaruga.php";
     require_once "Cobra.php";
+    require_once "Lobo.php";
 
     $mamifero = array();
     $reptil = array();
@@ -29,33 +30,42 @@
     $cobra = array();
     $goldfish = array();
     $arara = array();
+    $lobo = array();
 
 
-    $mamifero[0] = new Mamifero(85, 7, 4);
-    $mamifero[0]->setCorPelo("branco");
-    $mamifero[0]->locomover();
-    $mamifero[0]->alimentar();
-    $mamifero[0]->emitirSom();
-    print_r($mamifero[0]);
 
-    $reptil[0] = new Reptil(6.5, 3, 0);
-    $reptil[0]->locomover();
-    $reptil[0]->alimentar();
-    $reptil[0]->emitirSom();
-    print_r($reptil[0]);
+    //$mamifero[0] = new Mamifero(85, 7, 4);
+    //$mamifero[0]->setCorPelo("branco");
+    //$mamifero[0]->locomover();
+    //$mamifero[0]->alimentar();
+    //$mamifero[0]->emitirSom();
+    //print_r($mamifero[0]);
 
-    $kanguru[0] = new Kanguro(70, 8, 4);
-    $kanguru[0]->locomover();
-    $kanguru[0]->alimentar();
-    $kanguru[0]->emitirSom();
-    print_r($kanguru[0]);
+    //$reptil[0] = new Reptil(6.5, 3, 0);
+    //$reptil[0]->locomover();
+    //$reptil[0]->alimentar();
+    //$reptil[0]->emitirSom();
+    //print_r($reptil[0]);
+
+    //$kanguru[0] = new Kanguro(70, 8, 4);
+    //$kanguru[0]->locomover();
+    //$kanguru[0]->alimentar();
+    //$kanguru[0]->emitirSom();
+    //print_r($kanguru[0]);
 
     $cachorro[0] = new Cachorro(16, 3, 4);
     $cachorro[0]->locomover();
     $cachorro[0]->alimentar();
     $cachorro[0]->emitirSom();
+    $cachorro[0]->reagirIdadePeso();
 
-    print_r($cachorro[0]);
+    $lobo[0] = new Lobo(15, 3, 4);
+    $lobo[0]->locomover();
+    $lobo[0]->alimentar();
+    $lobo[0]->emitirSom();
+    
+
+    //print_r($cachorro[0]);
     ?>
     </pre>
 </body>
